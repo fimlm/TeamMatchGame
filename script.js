705161467613cb937b4d2015e774d75adb333634
monacho <monacho_4@hotmail.com>
2023-07-14 23:01:42 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -58,14 +58,13 @@ function calculateTeam() {
 // Función para mostrar la pregunta actual
 function showQuestion() {
     const questionElement = document.getElementById('questions');
-    questionElement.innerHTML = `
-        <label for="name">Nombre:</label>
-        <input type="text" id="name" value="${getNameFromStorage()}"><br><br>
+    questionElement.innerHTML = `        
+        <input type="text" placeholder=" Nombre" id="name" value="${getNameFromStorage()}"><br><br>
         <h2>${questions[currentQuestion].question}</h2>
-        <input type="radio" name="answer" value="A"> A
-        <input type="radio" name="answer" value="B"> B
+        <input onclick="calculateTeam()" type="radio" name="answer" value="A"> A
+        <input onclick="calculateTeam()" type="radio" name="answer" value="B"> B
         <br><br>
-        <button onclick="calculateTeam()">Siguiente pregunta</button>
+        
     `;
 }
 
