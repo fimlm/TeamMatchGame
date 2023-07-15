0876eea12068714e0568d6483f30a33ef361420e
angelosorno <angel@moratoo.com>
2023-07-14 23:37:35 -0500

    Setup update.sh

---
+++ .update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Commits Local Server Production
+git config pull.rebase true
+git add .
+git commit  -m "Server"
+
+# Actualizar el repositorio git desde la rama en posición
+git pull
+
+# Mostrar mensaje de finalización
+echo "El script se ha completado correctamente ✅"
